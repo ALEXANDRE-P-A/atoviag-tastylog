0238054f89e20903e23396080dee94f86f7b7a4c
Alexandre <alexandrepereiraassano@gmail.com>
2023-11-06 08:48:48 +0900

    template engine

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const app = express();
 app.set("view engine","ejs");
 app.disable("x-powered-by");
 
+// Dynamic resources.
 app.use("/", require("./routes/index.js"));
 
+// Execute application.
 app.listen(PORT,_=>{
   console.log(`Application listening at http://127.0.0.1:${PORT}`);
 });
