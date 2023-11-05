1840cf43d29a032552f7aa4d8009a6d0bf6feac6
Alexandre <alexandrepereiraassano@gmail.com>
2023-11-06 08:41:14 +0900

    ejs view engine

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
 
+// Express settings.
 const app = express();
+app.set("view engine","ejs");
+app.disable("x-powered-by");
 
-app.use("/",require("./routes/index.js"));
+app.use("/", require("./routes/index.js"));
 
 app.listen(PORT,_=>{
   console.log(`Application listening at http://127.0.0.1:${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-router.get("/",(req, res) => {
-  res.send("Welcome to Express Router.");
+router.get("/", (req, res) => {
+  res.render("./index.ejs");
 });
 
 module.exports = router;
