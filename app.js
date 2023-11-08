a7fbd2a9c962500459779f57fee36b9b86bfd0e6
Alexandre <alexandrepereiraassano@gmail.com>
2023-11-08 11:16:28 +0900

    set application logger

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 const favicon = require("serve-favicon");
+const logger = require("./lib/log/logger.js");
+const applicationlogger = require("./lib/log/applicationlogger.js");
+const accesslogger = require("./lib/log/accesslogger.js");
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,6 +12,9 @@ const app = express();
 app.set("view engine","ejs");
 app.disable("x-powered-by");
 
+// Set access logger.
+app.use(accesslogger());
+
 // Static resources.
 app.use("/public", express.static(path.join(__dirname, "/public")));
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
@@ -16,7 +22,10 @@ app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 // Dynamic resources.
 app.use("/", require("./routes/index.js"));
 
+// Set application logger.
+app.use(applicationlogger());
+
 // Execute application.
 app.listen(PORT,_=>{
-  console.log(`Application listening at http://127.0.0.1:${PORT}`);
+  logger.application.info(`Application listening at http://127.0.0.1:${PORT}`);
 });
---
+++ config/log4js.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const LOG_ROOT_DIR = process.env.LOG_ROOT_DIR || path.join(__dirname, "../logs");
+
+module.exports = {
+  appenders: {
+    consoleLogAppender : {
+      type: "console"
+    },
+    ApplicationLogAppender: {
+      type: "dateFile",
+      filename: path.join(LOG_ROOT_DIR, "./application.log"),
+      pattern: "yyyyMMdd",
+      numBackUp: 7
+    },
+    AccessLogAppender: {
+      type: "dateFile",
+      filename: path.join(LOG_ROOT_DIR, "./access.log"),
+      pattern: "yyyyMMdd",
+      numBackUp: 7
+    }
+  },
+  categories: {
+    "default": {
+      appenders : [
+        "consoleLogAppender"
+      ],
+      level: "ALL"
+    },
+    "application": {
+      appenders : [
+        "ApplicationLogAppender",
+        "consoleLogAppender"
+      ],
+      level: "INFO"
+    },
+    "access": {
+      appenders : [
+        "AccessLogAppender",
+        "consoleLogAppender"
+      ],
+      level: "INFO"
+    }
+  }
+};
