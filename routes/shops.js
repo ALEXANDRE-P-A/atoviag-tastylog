f03207011badbaf08eed3a9f3ce64d5e8eaceabc
Alexandre <alexandrepereiraassano@gmail.com>
2023-11-09 14:42:23 +0900

    shop page with reviews

--- routes/shops.js
+++ routes/shops.js
@@ -8,10 +8,15 @@ router.get("/:id", async (req, res, next) => {
     MySQLClient.executeQuery(
       await sql("SELECT_SHOP_DETAIL_BY_ID"),
       [id]
+    ),
+    MySQLClient.executeQuery(
+      await sql("SELECT_SHOP_REVIEW_BY_SHOP_ID"),
+      [id]
     )
   ]).then(results => {
     let data = results[0][0];
-    console.log(data);
+    data.reviews = results[1] || [];
+    console.log(data.reviews);
     res.render("./shops/index.ejs", data);
   }).catch(err => next(err));
 });
