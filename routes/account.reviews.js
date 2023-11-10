9d6ba0eda7b69893259dc25465a062ad71476c30
Alexandre <alexandrepereiraassano@gmail.com>
2023-11-10 13:07:24 +0900

    registration

--- routes/account.reviews.js
+++ routes/account.reviews.js
@@ -3,6 +3,21 @@ const { MySQLClient, sql } = require("../lib/database/client.js");
 const moment = require("moment");
 const DATE_FORMAT = "YYYY/MM/DD";
 
+let validateReviewData = req => {
+  let body = req.body;
+  let isValid = true, error = {};
+
+  if(body.visit && !moment(body.visit, DATE_FORMAT).isValid()){
+    isValid = false;
+    error.visit = "訪問日の日付文字列が不正です。";
+  }
+
+  if(isValid)
+    return undefined;
+
+  return error;
+};
+
 let createReviewData = req => {
   let body = req.body, date;
 
@@ -40,10 +55,50 @@ router.post("/regist/:shopId(\\d+)", async (req, res, next) => {
 });
 
 router.post("/regist/confirm", (req, res) => {
+  let error =validateReviewData(req);
   let review = createReviewData(req);
   let { shopId, shopName } = req.body;
 
+  if(error){
+    res.render("./account/reviews/regist-form.ejs", { error, shopId, shopName, review });
+  }
+
   res.render("./account/reviews/regist-confirm.ejs", { shopId, shopName, review });
+});
+
+router.post("/regist/execute", async (req, res, next) => {
+  let error = validateReviewData(req);
+  let review = createReviewData(req);
+  let { shopId, shopName } = req.body;
+  let userId = "999"; // TODO:ログイン実装後に更新
+  let transaction;
+
+  if(error){
+    res.render("./account/reviews/regist-form.ejs", { error, shopId, shopName, review });
+    return;
+  }
+
+  try {
+    transaction = await MySQLClient.beginTransaction();
+    transaction.executeQuery(
+      await sql("SELECT_SHOP_BY_ID_FOR_UPDATE"),
+      [shopId]
+    );
+    transaction.executeQuery(
+      await sql("INSERT_SHOP_REVIEW"),
+      [shopId, userId, review.score, review.visit, review.description]
+    );
+    transaction.executeQuery(
+      await sql("UPDATE_SHOP_SCORE_BY_ID"),
+      [shopId, shopId]
+    );
+    await transaction.commit();
+  } catch(err) {
+    await transaction.rollback();
+    next(err);
+  }
+
+  res.render("./account/reviews/regist-complete.ejs", { shopId });
 });
 
 module.exports = router;
